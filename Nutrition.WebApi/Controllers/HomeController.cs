e42ebbe8661aae90bc5646ded889fe15a22a7b3a
antonios123 <otr@abv.bg>
2013-08-30 15:44:53 +0300

    Calorimeter WebApi

---
+++ Nutrition.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Nutrition.WebApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Nutrition.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Nutrition.WebApi.WebApiApplication" Language="C#" %>
